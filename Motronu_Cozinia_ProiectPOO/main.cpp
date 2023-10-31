080857a58a06d78ef679d5a0c7831f6fd1170f58
Cozinia <hdwallpaper04@gmail.com>
2023-10-31 16:43:18 +0200

    Minor fixes + added comments

--- Motronu_Cozinia_ProiectPOO/main.cpp
+++ Motronu_Cozinia_ProiectPOO/main.cpp
@@ -13,7 +13,7 @@ private:
     int nrGenuriMuzicale;
     string* genuriMuzicale;
 public:
-
+    //Constructorul default
     Artist() : anAparitie(1776)
     {
         this->nume = "Wolfgang Amadeus Mozart";
@@ -27,6 +27,7 @@ public:
         }
         totalArtisti++;
     }
+    //Constructorul cu toti parametrii
     Artist(string nume, int varsta, float avereEuro, int nrGenuriMuzicale, string* genuriMuzicale, int anAparitie) : anAparitie(anAparitie)
     {
         this->nume = nume;
@@ -40,7 +41,7 @@ public:
         }
         totalArtisti++;
     }
-
+    //Constructorul cu 2 parametrii
     Artist(string nume, int varsta): anAparitie(1953)
     {
         this->nume = nume;
@@ -50,7 +51,7 @@ public:
         this->genuriMuzicale = nullptr;
         totalArtisti++;
     }
-
+    //Constructorul de copiere
     Artist& operator=(const Artist& artist)
     {
         if (this != &artist)
@@ -71,13 +72,6 @@ public:
         }
         return *this;
     }
-   
-    ~Artist() 
-    {
-        delete[] genuriMuzicale;
-        totalArtisti--;
-    }
-
     void afisare() 
     {
         cout << "Nume artist: " << this->nume << "\nVarsta: " << this->varsta << "\nAn aparitie: " << this->anAparitie << "\nAvere: " << this->avereEuro << "M euro\nGenuri muzicale: ";
@@ -87,63 +81,93 @@ public:
         }
         cout << "\n\n";
     }
-
-    const int getAnAparitie() const {
-        return anAparitie;
-    }
-
-    static int getTotalArtisti() {
+    //Getteri
+    static int getTotalArtisti() 
+    {
         return totalArtisti;
     }
-
-    string getNume() const {
-        return nume;
+    int getAnAparitie()
+    {
+        return this->anAparitie;
     }
-
-    int getVarsta() const {
-        return varsta;
+    string getNume()
+    {
+        return this->nume;
     }
-
-    float getAvereEuro() const {
-        return avereEuro;
+    int getVarsta()
+    {
+        return this->varsta;
     }
-
-    int getNrGenuriMuzicale() const {
-        return nrGenuriMuzicale;
+    float getAvereEuro()
+    {
+        return this->avereEuro;
     }
-
-    string* getGenuriMuzicale() const {
+    int getNrGenuriMuzicale()
+    {
+        return this->nrGenuriMuzicale;
+    }
+    string* getGenuriMuzicale()
+    {
         return this->genuriMuzicale;
     }
-
-    void setNume(const string& newNume) {
-        nume = newNume;
-    }
-
-    void setVarsta(int newVarsta) {
-        varsta = newVarsta;
-    }
-
-    void setAvereEuro(float newAvereEuro) {
-        this->avereEuro = newAvereEuro;
-    }
-
-    void setNrGenuriMuzicale(int newNrGenuriMuzicale) {
-        this->nrGenuriMuzicale = newNrGenuriMuzicale;
-    }
-
-    void setGenuriMuzicale(const string* newGenuriMuzicale) {
-        if (this->genuriMuzicale != NULL) {
-            delete[] this->genuriMuzicale;
-        }
-        this->genuriMuzicale = new string[this->nrGenuriMuzicale];
-        for (int i = 0; i < this->nrGenuriMuzicale; ++i) {
-            this->genuriMuzicale[i] = newGenuriMuzicale[i];
+    //Setteri
+    static void setTotalArtisti(int _totalAristi)
+    {
+        if (_totalAristi >= 0)
+        {
+            totalArtisti = _totalAristi;
         }
     }
-
+    void setNume(string _nume)
+    {
+        if (_nume != "" || _nume != " ")
+        {
+            this->nume = _nume;
+        }
+    }
+    void getVarsta(int _varsta)
+    {
+        if (_varsta > 0)
+        {
+            this->varsta = _varsta;
+        }
+    }
+    void setAvereEuro(float _avereEuro)
+    {
+        if (_avereEuro >= 0)
+        {
+            this->avereEuro = _avereEuro;
+        }
+    }
+    void setNrGenuriMuzicale(int _nrGenuriMuzicale)
+    {
+        if (_nrGenuriMuzicale > 0)
+        {
+            this->nrGenuriMuzicale = _nrGenuriMuzicale;
+        }
+    }
+   void setGenuriMuzicale(string* _genuriMuzicale)
+    {
+       if (_genuriMuzicale != NULL)
+       {
+           if (this->genuriMuzicale != NULL)
+           {
+               delete[]this->genuriMuzicale;
+           }
+           this->genuriMuzicale = new string[nrGenuriMuzicale];
+           for (int i = 0; i < nrGenuriMuzicale; i++)
+           {
+               this->genuriMuzicale[i] = _genuriMuzicale[i];
+           }
+       }
+    }
+    //Destructor
+    ~Artist()
+    {
+        delete[] genuriMuzicale;
+        totalArtisti--;
+    }
 };
-
 int Artist::totalArtisti = 0;
 
 class Album 
@@ -157,13 +181,12 @@ private:
     string* numeMelodii;
 
 public:
-
+    //Constructorul default
     Album() : anLansare(1982), nume("Thriller"), nrMelodii(0), pret(15.99), numeMelodii(nullptr)
     {
-        
 
     }
-
+    //Constructorul cu 2 parametrii
     Album(string nume, float pret): anLansare(1980)
     {
         this->nume = nume;
@@ -171,7 +194,7 @@ public:
         this->pret = pret;
         this->numeMelodii = new string[this->nrMelodii]{ "HellsBells", "ShootToThrill", "BackInBlack" };
     }
-
+    //Constructorul cu toti parametrii
     Album(string nume, int nrMelodii, float pret, string* numeMelodii): anLansare(2022)
     {
         this->nume = nume;
@@ -183,7 +206,7 @@ public:
             this->numeMelodii[i] = numeMelodii[i];
         }
     }
-
+    //Constructorul de copiere
     Album& operator=(Album& album)
     {
         if (this != &album)
@@ -204,63 +227,6 @@ public:
         }
         return *this;
     }
-    
-    ~Album() 
-    {
-        delete[] numeMelodii;
-    }
-
-    const int getAnLansare() const {
-        return anLansare;
-    }
-
-    static int getTVA() {
-        return TVA;
-    }
-
-    string getNume() const {
-        return nume;
-    }
-
-    int getNrMelodii() const {
-        return nrMelodii;
-    }
-
-    float getPret() const {
-        return pret;
-    }
-
-    string* getNumeMelodii() const {
-        return numeMelodii;
-    }
-
-    static void setTVA(int newTVA) {
-        TVA = newTVA;
-    }
-
-    void setNume(const string& newNume) {
-        nume = newNume;
-    }
-
-    void setNrMelodii(int newNrMelodii) {
-        nrMelodii = newNrMelodii;
-    }
-
-    void setPret(float newPret) {
-        pret = newPret;
-    }
-
-    void setNumeMelodii(const string* newNumeMelodii) {
-        if (this->numeMelodii != NULL)
-        {
-            delete[] this->numeMelodii;
-        }
-        numeMelodii = new string[nrMelodii];
-        for (int i = 0; i < nrMelodii; ++i) {
-            numeMelodii[i] = newNumeMelodii[i];
-        }
-    }
-
     void afisare()
     {
         cout << "Nume album: " << this->nume << "\nAn lansare: " << this->anLansare << "\nNumar melodii: " << this->nrMelodii << "\nPret: " << this->pret << " RON\nMelodii: ";
@@ -269,13 +235,87 @@ public:
         }
         cout << "\n\n";
     }
+    //Getteri
+    int getAnLansare()
+    {
+        return this->anLansare;
+    }
+    static int getTVA()
+    {
+        return TVA;
+    }
+    string getNume()
+    {
+        return this->nume;
+    }
+    int getNrMelodii()
+    {
+        return this->nrMelodii;
+    }
+    float getPret()
+    {
+        return this->pret;
+    }
+    string* getNumeMelodii()
+    {
+        return this->numeMelodii;
+    }
+    //Setteri
+    static void setTVA(int _TVA)
+    {
+        if (_TVA >=0)
+        {
+            TVA = _TVA;
+        }
+    }
+    void setNume(string _nume)
+    {
+        if (_nume != "" || _nume != " ")
+        {
+            this->nume = _nume;
+        }
+    }
+    void setNrMelodii(int _nrMelodii)
+    {
+        if (_nrMelodii > 0)
+        {
+            this->nrMelodii = _nrMelodii;
+        }
+    }
+    void setPret(float _pret)
+    {
+        if (_pret >= 0)
+        {
+            this->pret = _pret;
+        }
+    }
+    void setNumeMelodii(string* _numeMelodii)
+    {
+        if (_numeMelodii != NULL)
+        {
+            if (this->numeMelodii != NULL)
+            {
+                delete[] this->numeMelodii;
+            }
+            this->numeMelodii = new string[this->nrMelodii];
+            for (int i = 0; i < this->nrMelodii; i++)
+            {
+                this->numeMelodii[i] = _numeMelodii[i];
+            }
+        }
+    }
+    //Destructor
+    ~Album()
+    {
+        delete[] numeMelodii;
+    }
+
 };
 int Album::TVA = 19;
 
-
-
 class Melodie {
 private:
+
     const float durata;
     static int nrTotalMelodii;
     string nume;
@@ -283,7 +323,7 @@ private:
     int nrVersuri;
     string* versuri;
 public:
-
+    //Constructorul default
     Melodie() : durata(4.0)
     {
         this->nume = "Hello";
@@ -292,7 +332,7 @@ public:
         this->versuri = new std::string[this->nrVersuri]{ "VersulNr1", "VersulNr2" };
         nrTotalMelodii++;
     }
-
+    //Constructorul cu 2 parametrii
     Melodie(float durata, string nume) : durata(durata)
     {
         this->nume = nume;
@@ -302,7 +342,7 @@ public:
 
         nrTotalMelodii++;
     }
-
+    //Constructorul cu toti parametrii
     Melodie(float durata, string nume, string numeArtist, int nrVersuri, string* versuriArray): durata(durata), nume(nume), numeArtist(numeArtist), nrVersuri(nrVersuri), versuri(new string[nrVersuri])
     {
         for (int i = 0; i < nrVersuri; ++i) 
@@ -311,7 +351,7 @@ public:
         }
         nrTotalMelodii++;
     }
-
+    //Constructorul de copiere
     Melodie& operator=(Melodie& melodie)
     {
         if (this != &melodie)
@@ -331,60 +371,75 @@ public:
         }
         return *this;
     }
-    
-    ~Melodie() 
+    //Getteri
+    float getDurata()
     {
-        delete[] versuri;
-        nrTotalMelodii--;
+        return this->durata;
     }
-
-    float getDurata() const {
-        return durata;
-    }
-
-    static int getNrTotalMelodii() {
+    static int getNrTotalMelodii()
+    {
         return nrTotalMelodii;
     }
-
-    string getNume() const {
-        return nume;
+    string getNume()
+    {
+        return this->nume;
     }
-
-    string getNumeArtist() const {
-        return numeArtist;
+    string getNumeArtist()
+    {
+        return this->numeArtist;
     }
-
-    int getNrVersuri() const {
-        return nrVersuri;
+    int getNrVersuri()
+    {
+        return this->nrVersuri;
     }
-
-    string* getVersuri() const {
-        return versuri;
+    string* getVersuri()
+    {
+        return this->versuri;
     }
-
-    void setNume(const string& newNume) {
-        nume = newNume;
-    }
-
-    void setNumeArtist(const string& newNumeArtist) {
-        numeArtist = newNumeArtist;
-    }
-
-    void setNrVersuri(int newNrVersuri) {
-        nrVersuri = newNrVersuri;
-    }
-
-    void setVersuri(const string* newVersuri) {
-        if (this->versuri != NULL)
+    //Setteri
+    static void setNrtotalMelodii(int _nrTotalMelodii)
+    {
+        if (_nrTotalMelodii >=0)
         {
-            delete[] this->versuri;
-        }
-        versuri = new string[nrVersuri];
-        for (int i = 0; i < nrVersuri; ++i) {
-            versuri[i] = newVersuri[i];
+            nrTotalMelodii = _nrTotalMelodii;
         }
     }
-
+    void setNume(string _nume)
+    {
+        if (_nume != "" || _nume != " ")
+        {
+            this->nume = _nume;
+        }
+    }
+    void setNumeArtist(string _numeArtist)
+    {
+        if (_numeArtist != "" || _numeArtist != " ")
+        {
+            this->numeArtist = _numeArtist;
+        }
+    }
+    void setNrVersuri(int _nrVersuri)
+    {
+        if (_nrVersuri > 0)
+        {
+            this->nrVersuri = _nrVersuri;
+        }
+    }
+    void setVersuri(string* _versuri)
+    {
+        if (_versuri != NULL)
+        {
+            if (this->versuri != NULL)
+            {
+                delete[] this->versuri;
+            }
+            this->versuri = new string[this->nrVersuri];
+            for (int i = 0; i < this->nrVersuri; i++)
+            {
+                this->versuri[i] = _versuri[i];
+            }
+        }
+    }
     void afisare()
     {
         cout << "Nume melodie: " << this->nume << "\nNume artist: " << this->numeArtist << "\nDurata: " << this->durata << " minute\nVersuri:\n";
@@ -394,18 +449,29 @@ public:
         }
         cout << endl;
     }
+    //Destructor
+    ~Melodie()
+    {
+        delete[] versuri;
+        nrTotalMelodii--;
+    }
 };
 int Melodie::nrTotalMelodii = 0;
 
 
 int main() 
 {
-
+    //Artist
     string adeleGenuriMuzicale[] = { "Pop", "Rock" };
+    //Apelarea constructorului cu toti parametrii
     Artist adele("Adele", 35, 220.53, 2, adeleGenuriMuzicale, 2006);
+    //Apelarea constructorului cu 2 parametrii
     Artist elvis("Elvis Presley", 40);
+    //Apelarea constructorului default
     Artist artistCult;
+
     Artist copyAdele;
+    //Apelarea constructorului de copiere
     copyAdele = adele;
 
     adele.afisare();
@@ -414,38 +480,43 @@ int main()
     cout << "Copy constructor:\n";
     copyAdele.afisare();
 
-    cout << "Total artisti: " << Artist::getTotalArtisti() << endl << endl;
-
-
+    //Getteri si setteri
     Artist artistGetSet;
-
-    artistGetSet.setNume("Freddie Mercury");
-    artistGetSet.setVarsta(45);
-    artistGetSet.setAvereEuro(100.5);
+    //Set
+    artistGetSet.setNume("Bruno Mars");
+    artistGetSet.setAvereEuro(30.2);
     artistGetSet.setNrGenuriMuzicale(2);
     artistGetSet.setGenuriMuzicale(adeleGenuriMuzicale);
 
+    //Get
+    cout << "Artist get & set:" << endl;
     cout << "Nume artist: " << artistGetSet.getNume() << endl;
     cout << "Varsta: " << artistGetSet.getVarsta() << endl;
     cout << "An aparitie: " << artistGetSet.getAnAparitie() << endl;
     cout << "Avere: " << artistGetSet.getAvereEuro() << "M euro" << endl;
     cout << "Nr genuri muzicale: " << artistGetSet.getNrGenuriMuzicale() << endl;
-    cout << "Genuri muzicale: " << artistGetSet.getGenuriMuzicale() << endl;
+    string* artistGenres = artistCult.getGenuriMuzicale();
+    cout << "Genuri muzicale: ";
+    for (int i = 0; i < artistCult.getNrGenuriMuzicale(); i++)
+    {
+        cout << artistGenres[i] << " ";
+    }
 
-    cout << "\nArtist prin get-eri si sett-eri:\n";
-    artistGetSet.afisare();
+    cout << "\n\nTotal artisti: " << Artist::getTotalArtisti() << endl << endl;
 
 
 
+    //Album
     string numeMelodii[] = { "SpeakToMe", "Breathe(InTheAir)", "BrainDamage" };
-
+    //Apelarea constructorului default
     Album albumulUnu;
-
+    //Apelarea constructorului cu toti parametrii
     Album albumulDoi("Back in Black", 29.99);
-
+    //Apelarea constructorului cu toti parametrii
     Album albumulTrei("The Dark Side of the Moon", 3, 39.99, numeMelodii);
 
     Album copyAlbumUnu;
+    //Apelarea constructorului de copiere
     copyAlbumUnu = albumulUnu;
 
     albumulUnu.afisare();
@@ -454,63 +525,65 @@ int main()
     cout << "Copy constructor:\n";
     copyAlbumUnu.afisare();
 
-    cout << "TVA: " << Album::getTVA() << "%" << endl;
-
-
+    //Getteri si setteri
     Album albumGetSet;
-
+    //Set
     albumGetSet.setNume("To the future");
     albumGetSet.setNrMelodii(10);
     albumGetSet.setPret(25.99);
     albumGetSet.setNrMelodii(3);
     albumGetSet.setNumeMelodii(numeMelodii);
-
-
+    //Get
+    cout << "Album get & set:" << endl;
     cout << "Nume album: " << albumGetSet.getNume() << endl;
     cout << "An lansare: " << albumGetSet.getAnLansare() << endl;
     cout << "Pret: " << albumGetSet.getPret() << " RON" << endl;
     cout << "Nr. melodii: " << albumGetSet.getNrMelodii() << endl;
-    cout << "Melodii: " << albumGetSet.getNumeMelodii() << endl;
+    string* album = albumGetSet.getNumeMelodii();
+    cout << "Melodii ";
+    for (int i = 0; i < albumGetSet.getNrMelodii(); i++)
+    {
+        cout << album[i] << " ";
+    }
 
-    cout << "\nAlbum prin get-eri si sett-eri:\n";
-    albumGetSet.afisare();
+    cout << "\n\nTVA: " << Album::getTVA() << "%" << endl << endl;
 
 
-
+    //Melodie
     string versuriArray[] = { "I'm Mr. Originality", "Come my way, come and see", "Let me be your fantasy" };
-
+    //Apelarea constructorului default
     Melodie melodieImplicita;
 
+    //Apelarea constructorului cu 2 parametrii
     Melodie melodieCuDurataNume(4.10, "TurningTables");
-
+    //Apelarea constructorului cu toti parametrii
     Melodie mrOriginality(3.20,  "MrOriginality", "Simplu", 3, versuriArray);
+
     Melodie copyMelodieImplicita;
+    //Apelarea constructorului de copiere
     copyMelodieImplicita = melodieImplicita;
 
-    melodieImplicita.afisare();
-    melodieCuDurataNume.afisare();
-    mrOriginality.afisare();
-    cout << "Copy constructor:\n";
-    copyMelodieImplicita.afisare();
-
-    cout << "Total Melodii: " << Melodie::getNrTotalMelodii() << endl;
-
+    //Getteri si setteri
     Melodie melodieGetSet;
-
+    //Set
     melodieGetSet.setNume("Rolling in the Deep");
     melodieGetSet.setNumeArtist("Adele Adkins");
     melodieGetSet.setNrVersuri(3);
     melodieGetSet.setVersuri(versuriArray);
-
+    //Get
+    cout << "Melodie get & set:" << endl;
     cout << "Nume melodie: " << melodieGetSet.getNume() << endl;
     cout << "Nume artist: " << melodieGetSet.getNumeArtist() << endl;
     cout << "Durata: " << melodieGetSet.getDurata() << " minute" << endl;
     cout << "Nr. versuri: " << melodieGetSet.getNrVersuri() << endl;
-    cout << "Versuri: " << melodieGetSet.getVersuri() << endl;
+    string* melodie = melodieGetSet.getVersuri();
+    cout << "Versuri ";
+    for (int i = 0; i < melodieGetSet.getNrVersuri(); i++)
+    {
+        cout << melodie[i] << "\n";
+    }
 
-    cout << "\nMelodie prin get-eri si sett-eri:\n";
-    melodieGetSet.afisare();
-
+    cout << "\n\nTotal Melodii: " << Melodie::getNrTotalMelodii() << endl;
 
 
     return 0;
